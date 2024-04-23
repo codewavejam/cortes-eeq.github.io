fa22bdcfb49258a15319109d905b821d9f52f9aa
codewavejam <167836097+codewavejam@users.noreply.github.com>
2024-04-23 17:57:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById('queryForm').addEventListener('submit', function(event) 
     event.preventDefault();
     var searchId = document.getElementById('searchId').value;
     if (searchId === '') {
-        alert('Porfavor ingrese los datos requeridos'); // Alert user if input is empty
+        alert('Ingrese los datos requeridos'); // Alert user if input is empty
         return; // Exit function if input is empty
     }
     var url = 'https://yzikcmv0ba.execute-api.us-east-1.amazonaws.com/default/srv-plate-restriction-by-header';
