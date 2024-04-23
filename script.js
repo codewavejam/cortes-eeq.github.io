d060e932b0619f7958410148606e14caa9fe4cc5
codewavejam <167836097+codewavejam@users.noreply.github.com>
2024-04-22 21:41:09 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+document.getElementById('queryForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    var searchId = document.getElementById('searchId').value;
+    var url = 'https://yzikcmv0ba.execute-api.us-east-1.amazonaws.com/default/srv-plate-restriction-by-header';
+
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ cuenid: searchId })
+    })
+    .then(response => response.text())
+    .then(data => {
+        var resultElement = document.getElementById('result');
+            resultElement.innerText = data;
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        var resultElement = document.getElementById('result');
+        resultElement.innerText = "An error occurred while fetching data.";
+    });
+});
