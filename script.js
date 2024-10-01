5997adcf1a17fc7c283fabdeba4b90fc797ca090
CodeWave <codewavejam@proton.me>
2024-09-30 21:16:37 -0500

    Add loader to query

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 document.getElementById('queryForm').addEventListener('submit', function(event) {
     event.preventDefault();
     var searchId = document.getElementById('searchId').value;
+    var resultElement = document.getElementById('result');
+    var loader = document.getElementById('loader');
+
     if (searchId === '') {
         alert('Ingrese los datos requeridos'); // Alert user if input is empty
         return; // Exit function if input is empty
     }
     var url = 'https://yzikcmv0ba.execute-api.us-east-1.amazonaws.com/default/srv-plate-restriction-by-header';
 
+    loader.style.display = 'block';
+    resultElement.innerText = '';
     fetch(url, {
         method: 'POST',
         headers: {
@@ -16,12 +21,13 @@ document.getElementById('queryForm').addEventListener('submit', function(event) 
     })
     .then(response => response.text())
     .then(data => {
-        var resultElement = document.getElementById('result');
-            resultElement.innerText = data;
+        resultElement.innerText = data;
     })
     .catch(error => {
         console.error('Error:', error);
-        var resultElement = document.getElementById('result');
         resultElement.innerText = "An error occurred while fetching data.";
+    })
+    .finally(() => {
+        loader.style.display = 'none'; // Ocultar el loader
     });
 });
